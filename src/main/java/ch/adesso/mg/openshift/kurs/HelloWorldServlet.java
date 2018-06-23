a4ebedb42e9a15788277b0a726b7675be0aab9fe
MG <gabor.moos@adesso.ch>
2018-06-23 15:42:47 +0200

    moved to helloshift

--- src/main/java/ch/adesso/mg/openshift/kurs/HelloWorldServlet.java
+++ src/main/java/ch/adesso/mg/openshift/kurs/HelloWorldServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/helloshift")
 public class HelloWorldServlet extends HttpServlet{
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
