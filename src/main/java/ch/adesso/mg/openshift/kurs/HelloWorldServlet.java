4a9a002e3fdda9201bb04b874ca033542e7b62c6
MG <gabor.moos@adesso.ch>
2018-06-23 14:15:09 +0200

    second commit

---
+++ src/main/java/ch/adesso/mg/openshift/kurs/HelloWorldServlet.java
@@ -0,0 +1,15 @@
+package ch.adesso.mg.openshift.kurs;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloWorldServlet extends HttpServlet{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().print("Hello Openshift");
+    }
+}
